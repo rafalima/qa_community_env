2d72ecc29081a46a46eed59f6108ccca5e4b1ce0
Rafael Lima <rlima@thoughtworks.com>
2013-12-23 13:35:11 -0300

    adding the qa community env structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QaCommunityEnv::Application.config.secret_key_base = 'a27e072adf79081c2effa02bfb22b9b0f3570f53ecd9d1785bba362d5077bafa2680c8ca39c76464e20ceb0410eb89e033bff4efcb525d302fddab612316a184'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QaCommunityEnv::Application.config.session_store :cookie_store, key: '_qa_community_env_session'

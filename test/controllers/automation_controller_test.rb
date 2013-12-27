39a05f6c9ff7f83f55cd0372d40fb05fc965e680
Rafael Lima <rlima@thoughtworks.com>
2013-12-27 08:55:09 -0300

    adding automation controller

---
+++ test/controllers/automation_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AutomationControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/automation_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class AutomationHelperTest < ActionView::TestCase
+end
